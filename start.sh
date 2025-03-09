197740ffd747059353283caa2fd46fb50bf7f196
joyel24 <joel.martin.2104@gmail.com>
2025-03-09 05:28:17 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 cd /meshtastic
 source bin/activate
 cd firmware
+git checkout master
 git submodule update --init --recursive --remote
 git pull
 cd lib/device-ui/
+git checkout master
 git pull
 cd ../..
 /root/.platformio/penv/bin/pio run -e t-deck-tft
